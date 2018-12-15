9c2c29024956f42d9aad8d9afd3f963fdf1018f1
EJ <90cej@naver.com>
2018-12-15 19:43:40 +0900

    completed sticky header functionality.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -21,8 +21,10 @@ jane.greet();
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
 //var revealOnScroll = new RevealOnScroll();
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.testimonial'), '50%');
+var stickyHeader = new StickyHeader();
